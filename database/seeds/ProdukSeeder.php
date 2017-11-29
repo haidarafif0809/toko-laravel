6b9a7dc9f61759170272971105046f3bdd41980b
Muhammad Hari <Nrmuhamad@gmail.com>
2017-11-29 13:44:12 +0700

    Membuang kolom yang tidak perlu pada master data produk

--- database/seeds/ProdukSeeder.php
+++ database/seeds/ProdukSeeder.php
@@ -15,21 +15,13 @@ class ProdukSeeder extends Seeder
         // sample produk 1
     	$produk = new Produk();
     	$produk->kode_produk = "0018";
-    	$produk->kode_barcode = "1234567";
     	$produk->nama_produk = "Beras 10kg";
     	$produk->harga_beli = "85000";
     	$produk->harga_jual = "90000";
     	$produk->satuans_id = "1";
     	$produk->kategori_produks_id = "1";
-    	$produk->status = "Aktif";
-    	$produk->supliers_id = "1";
-    	$produk->limit_stok = "0";
-    	$produk->over_stok = "10";
+    	$produk->status_jual = "Aktif";
     	$produk->berkaitan_dgn_stok = "1000";
-    	$produk->jenis_produk = "Produk";
-    	$produk->tipe_produk = "Produk";
-    	$produk->golongan = "Produk";
-    	$produk->golongan_produk = "Produk";
     	$produk->save();
 
 
@@ -41,15 +33,8 @@ class ProdukSeeder extends Seeder
     	$produk->harga_jual = "115000";
     	$produk->satuans_id = "5";
     	$produk->kategori_produks_id = "1";
-    	$produk->status = "Aktif";
-    	$produk->supliers_id = "2";
-    	$produk->limit_stok = "0";
-    	$produk->over_stok = "10";
+    	$produk->status_jual = "Aktif";
     	$produk->berkaitan_dgn_stok = "1000";
-    	$produk->jenis_produk = "Produk";
-    	$produk->tipe_produk = "Produk";
-    	$produk->golongan = "Produk";
-    	$produk->golongan_produk = "Produk";
     	$produk->save();
 
               // sample produk 3
@@ -60,15 +45,8 @@ class ProdukSeeder extends Seeder
     	$produk->harga_jual = "417700";
     	$produk->satuans_id = "8";
     	$produk->kategori_produks_id = "1";
-    	$produk->status = "Aktif";
-    	$produk->supliers_id = "3";
-    	$produk->limit_stok = "0";
-    	$produk->over_stok = "10";
+    	$produk->status_jual = "Aktif";
     	$produk->berkaitan_dgn_stok = "1000";
-    	$produk->jenis_produk = "Produk";
-    	$produk->tipe_produk = "Produk";
-    	$produk->golongan = "Produk";
-    	$produk->golongan_produk = "Produk";
     	$produk->save();
     }
 }
