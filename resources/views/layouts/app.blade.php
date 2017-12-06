09633fbcee742d4b02b7d40e111f34a6d0138c60
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-12-06 13:57:49 +0700

    Menginstall packge jens seger

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="utf-8">
-    <meta content="IE=edge" http-equiv="X-UA-Compatible">
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <!-- CSRF Token -->
-    <meta content="{{ csrf_token() }}" name="csrf-token">
-    <title>
-        Toko Dasar
-    </title>
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<!-- CSRF Token -->
+	<meta content="{{ csrf_token() }}" name="csrf-token">
+	<title>
+		Toko Dasar
+	</title>
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </link>
 </link>
 </meta>
@@ -20,42 +20,42 @@
 </meta>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <!-- Collapsed Hamburger -->
-                    <button aria-expanded="false" class="navbar-toggle collapsed" data-target="#app-navbar-collapse" data-toggle="collapse" type="button">
-                        <span class="sr-only">
-                            Toggle Navigation
-                        </span>
-                        <span class="icon-bar">
-                        </span>
-                        <span class="icon-bar">
-                        </span>
-                        <span class="icon-bar">
-                        </span>
-                    </button>
-                    <!-- Branding Image -->
-                    <router-link :to="{name: 'indexDashboard'}" class="navbar-brand">
-                        Toko Dasar
-                    </router-link>
-                </div>
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li>
-                            <router-link :to="{name: 'indexDashboard'}">
-                                Dashboard
-                            </router-link>
-                        </li>
-                        <li class="dropdown">
-                            <a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-                                Produk
-                                <span class="caret">
-                                </span>
-                            </a>
-                            <ul class="dropdown-menu">
+	<div id="app">
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<!-- Collapsed Hamburger -->
+					<button aria-expanded="false" class="navbar-toggle collapsed" data-target="#app-navbar-collapse" data-toggle="collapse" type="button">
+						<span class="sr-only">
+							Toggle Navigation
+						</span>
+						<span class="icon-bar">
+						</span>
+						<span class="icon-bar">
+						</span>
+						<span class="icon-bar">
+						</span>
+					</button>
+					<!-- Branding Image -->
+					<router-link :to="{name: 'indexDashboard'}" class="navbar-brand">
+						Toko Dasar
+					</router-link>
+				</div>
+				<div class="collapse navbar-collapse" id="app-navbar-collapse">
+					<!-- Left Side Of Navbar -->
+					<ul class="nav navbar-nav">
+						<li>
+							<router-link :to="{name: 'indexDashboard'}">
+								Dashboard
+							</router-link>
+						</li>
+						<li class="dropdown">
+							<a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
+								Produk
+								<span class="caret">
+								</span>
+							</a>
+							<ul class="dropdown-menu">
 
                                 <!-- <li>
                                     <router-link :to="{name: 'indexSuplier'}">
@@ -68,15 +68,15 @@
                                     </router-link>
                                 </li> -->
                                 <li>
-                                    <router-link :to="{name: 'indexKategoriProduk'}">
-                                        Kategori Produk
-                                    </router-link>
+                                	<router-link :to="{name: 'indexKategoriProduk'}">
+                                		Kategori Produk
+                                	</router-link>
                                 </li>
 
                                 <li>
-                                    <router-link :to="{name: 'indexProduk'}">
-                                        Produk
-                                    </router-link>
+                                	<router-link :to="{name: 'indexProduk'}">
+                                		Produk
+                                	</router-link>
                                 </li>
                                 <!-- <li>
                                     <router-link :to="{name: 'indexKas'}">
@@ -91,22 +91,22 @@
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-                                Transaksi
-                                <span class="caret">
-                                </span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <router-link :to="{name: 'indexKasMasuk'}">
-                                        Kas Masuk
-                                    </router-link>
-                                </li>
-                                <li>
-                                    <router-link :to="{name: 'indexKasKeluar'}">
-                                        Kas Keluar
-                                    </router-link>
-                                </li>
+                        	<a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
+                        		Transaksi
+                        		<span class="caret">
+                        		</span>
+                        	</a>
+                        	<ul class="dropdown-menu">
+                        		<li>
+                        			<router-link :to="{name: 'indexKasMasuk'}">
+                        				Kas Masuk
+                        			</router-link>
+                        		</li>
+                        		<li>
+                        			<router-link :to="{name: 'indexKasKeluar'}">
+                        				Kas Keluar
+                        			</router-link>
+                        		</li>
                                <!--  <li>
                                     <router-link :to="{name: 'indexUser'}">
                                         Kas Mutasi
@@ -116,61 +116,61 @@
                         </li>
 
                         <li>
-                            <router-link :to="{name: 'indexUser'}">
-                                Staff
-                            </router-link>
+                        	<router-link :to="{name: 'indexUser'}">
+                        		Staff
+                        	</router-link>
                         </li>
 
                         <li>
-                            <router-link :to="{name: 'indexPelanggan'}">
-                                Pelanggan
-                            </router-link>
+                        	<router-link :to="{name: 'indexPelanggan'}">
+                        		Pelanggan
+                        	</router-link>
                         </li>
 
                         <li>
-                            <router-link :to="{name: 'indexDashboard'}">
-                                Penjualan
-                            </router-link>
+                        	<router-link :to="{name: 'indexDashboard'}">
+                        		Penjualan
+                        	</router-link>
                         </li>
 
                         <li>
-                            <router-link :to="{name: 'indexDashboard'}">
-                                Laporan
-                            </router-link>
+                        	<router-link :to="{name: 'indexDashboard'}">
+                        		Laporan
+                        	</router-link>
                         </li>
                     </ul>
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li>
-                            <a href="{{ route('login') }}">
-                                Login
-                            </a>
-                        </li>
-                        <li>
-                            <a href="{{ route('register') }}">
-                                Register
-                            </a>
-                        </li>
-                        @else
-                        <li class="dropdown">
-                            <a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-                                {{ Auth::user()->name }}
-                                <span class="caret">
-                                </span>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
+                    	<!-- Authentication Links -->
+                    	@guest
+                    	<li>
+                    		<a href="{{ route('login') }}">
+                    			Login
+                    		</a>
+                    	</li>
+                    	<li>
+                    		<a href="{{ route('register') }}">
+                    			Register
+                    		</a>
+                    	</li>
+                    	@else
+                    	<li class="dropdown">
+                    		<a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
+                    			{{ Auth::user()->name }}
+                    			<span class="caret">
+                    			</span>
+                    		</a>
+                    		<ul class="dropdown-menu">
+                    			<li>
+                    				<a href="{{ route('logout') }}" onclick="event.preventDefault();
+                    				document.getElementById('logout-form').submit();">
+                    				Logout
+                    			</a>
+                    			<form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
+                    				{{ csrf_field() }}
+                    			</form>
+                    		</li>
+                    	</ul>
                     </li>
                     @endguest
                 </ul>
