6e572c059bfbda1b5750412b2ebef3123ab1cbd4
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-12-11 14:58:52 +0700

    Membuat proses untuk menampilkan data kategoriproduk

--- resources/assets/js/components/penjualan/PenjualanIndex.vue
+++ resources/assets/js/components/penjualan/PenjualanIndex.vue
@@ -13,7 +13,7 @@
 								<table class="table table-striped">
 									<tbody v-if="penjualans.length > 0 && loading == false" class="data-ada">
 										<tr v-for="penjualan ,index in penjualans">
-											<td class="nav-link">{{penjualan.nama_kategori_produk}}</td>
+											<td class="nav-link">{{kategoriproduk.nama_kategori_produk}}</td>
 										</tr>
 									</tbody>
 									<tbody v-else-if="loading == true" class="data-ada">
@@ -43,3 +43,39 @@
 </div>
 </div>
 </template>
+<script>
+export default {
+	data: function () {
+		return {
+			penjualans: [],
+			penjualansData: {},
+			url : window.location.origin + (window.location.pathname).replace("home", "penjualan"),
+			loading : true
+		}
+	},
+
+	mounted() {
+		var app = this;
+		app.getPenjualans();
+	},
+	methods: {
+		getPenjualans(page) {
+			var app = this;
+			if (typeof page === 'undefined') {
+				page = 1;
+			}
+
+			axios.get(app.url+'/view?page='+page)
+			.then(function (resp) {
+				app.penjualans = resp.data.data;
+				app.penjualansData = resp.data;
+				app.loading = false;
+			})
+			.catch(function (resp) {
+				alert("Could not load penjualans");
+				app.loading = false
+			});
+		},
+	}
+}
+</script>
