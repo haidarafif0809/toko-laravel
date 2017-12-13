bba519480210f60dcfa424fb98d87b0303a2082b
Muhammad Hari <Nrmuhamad@gmail.com>
2017-12-14 04:13:11 +0700

    Memperbaiki bug ketika inputan foto dikosongkan pada produk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,21 +19,27 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
-            $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
-            if (in_array($type, $parameters)) {
-                return true;
+            if (!empty($value) && !empty($parameters)) {
+                $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
+
+                if (in_array($type, $parameters)) {
+                    return true;
+                }
+                return false;
+            } else {
+                return false;
             }
-            return false;
         });
+
+        Validator::replacer('image64', function ($message, $attribute, $rule, $parameters) {
+            return str_replace(':values', join(",", $parameters), $message);
+        });
+
         KasMasuk::observe(KasMasukObserver::class);
         // untuk menyesuaikan waktu berdasarkan tempat
         \Carbon\Carbon::setlocale('id');
         // jika ingin menesuaikan dengan locale di laravel
         // \Carbon\Carbon::setLocale(config('app.locale'));
-
-        Validator::replacer('image64', function ($message, $attribute, $rule, $parameters) {
-            return str_replace(':values', join(",", $parameters), $message);
-        });
     }
 
     /**
