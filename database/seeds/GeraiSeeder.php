df4dcffc43e0217bd0161b67fdce01641ca19ece
Muhammad Khoirul Anwar <mkhoirulanwar41@gmail.com>
2017-12-29 11:21:53 +0700

    Memperbaiki validasi di controller

--- database/seeds/GeraiSeeder.php
+++ database/seeds/GeraiSeeder.php
@@ -25,30 +25,6 @@ class GeraiSeeder extends Seeder
             'meja'         => '1',
 
         ]);
-        $tambah_gerai2 = Gerai::create([
-            'nama_gerai'   => 'Athacom 1',
-            'alamat_gerai' => 'jepara',
-            'kota'         => 'Lampung',
-            'no_telepon_a' => '081369050112',
-            'no_telepon_b' => '081369050115',
-            'notes'        => 'Notes1',
-            'nama_pajak'   => 'pajak charge',
-            'rasio'        => '10',
-            'meja'         => '1',
-
-        ]);
-        $tambah_gerai3 = Gerai::create([
-            'nama_gerai'   => 'Athacom 1',
-            'alamat_gerai' => 'jepara',
-            'kota'         => 'Lampung',
-            'no_telepon_a' => '081369050112',
-            'no_telepon_b' => '081369050115',
-            'notes'        => 'Notes1',
-            'nama_pajak'   => 'pajak charge',
-            'rasio'        => '10',
-            'meja'         => '1',
-
-        ]);
 
     }
 }
