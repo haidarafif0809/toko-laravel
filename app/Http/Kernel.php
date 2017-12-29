c259fb81a192f236cf0ca736764c0690ed4a8534
khofiq <khoirulgeong123@gmail.com>
2017-12-29 13:43:20 +0700

    menambah midleware user-should-aktif
    untuk membuat tombol aktif/nonaktif pada master data toko

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,15 +51,17 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // status user harus aktif
+        'user-should-aktif' => \App\Http\Middleware\UserShouldAktif::class,
+        'auth'              => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth.basic'        => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'bindings'          => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'can'               => \Illuminate\Auth\Middleware\Authorize::class,
+        'guest'             => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle'          => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         //ROLE
-        'role' => \Laratrust\Middleware\LaratrustRole::class,
-        'permission' => \Laratrust\Middleware\LaratrustPermission::class,
-        'ability' => \Laratrust\Middleware\LaratrustAbility::class
+        'role'              => \Laratrust\Middleware\LaratrustRole::class,
+        'permission'        => \Laratrust\Middleware\LaratrustPermission::class,
+        'ability'           => \Laratrust\Middleware\LaratrustAbility::class,
     ];
 }
