a7104ef1a41f1475de3e617fff17acfa6641ff65
Muhammad Hari <Nrmuhamad@gmail.com>
2018-01-08 15:34:07 +0700

    Menyelesaikan validasi excel dan validasi yang membuat kategori produk baru
    Memperbaiki validasi untuk upload excel pada produk dan menyelesaikan validasi yang akan membuat kategori produk baru ketika kategori produk yang user masukkan pada excel tidak ada

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,6 +6,7 @@ use App\KategoriProduk;
 use App\Produk;
 use Auth;
 use Carbon\Carbon;
+use DB;
 use Excel;
 use File;
 use Illuminate\Http\Request;
@@ -109,7 +110,7 @@ class ProdukController extends Controller
                 'nama_produk'         => 'required',
                 'kategori_produks_id' => 'required|exists:kategori_produks,id',
                 'harga_beli'          => 'required|numeric',
-                'harga_jual'          => 'numeric',
+                'harga_jual'          => 'required|numeric',
                 'status_jual'         => 'required',
             ]);
 
@@ -168,7 +169,7 @@ class ProdukController extends Controller
             'nama_produk'         => 'required',
             'kategori_produks_id' => 'required|exists:kategori_produks,id',
             'harga_beli'          => 'required|numeric',
-            'harga_jual'          => 'numeric',
+            'harga_jual'          => 'required|numeric',
             'status_jual'         => 'required',
         ]);
 
@@ -261,40 +262,125 @@ class ProdukController extends Controller
         // rule untuk validasi setiap row pada file excel
 
         $rowRules = [
-            'Kode Produk'     => 'required',
+            'Kode Produk'     => 'required|unique:produks,kode_produk',
             'Nama Produk'     => 'required',
-            'Kategori Produk' => 'required',
-            'Harga Beli'      => 'required',
-            'Harga Jual'      => 'required',
+            'Kategori Produk' => 'required|exists:kategori_produks,id',
+            'Harga Beli'      => 'required|numeric',
+            'Harga Jual'      => 'required|numeric',
             'Bisa Dijual'     => 'required',
         ];
         // Catat semua id buku baru
         // ID ini kita butuhkan untuk menghitung total buku yang berhasil diimport
         $produk_id = [];
+
         // looping setiap baris, mulai dari baris ke 2 (karena baris ke 1 adalah nama kolom)
         foreach ($excels as $row) {
             // Membuat validasi untuk row di excel
             // Disini kita ubah baris yang sedang di proses menjadi array
             $validator = Validator::make($row->toArray(), $rowRules);
 
-            // buat buku baru
-            $produk = Produk::create([
-                'kode_produk'     => $row['kode_produk'],
-                'nama_produk'     => $row['nama_produk'],
-                'kategori_produk' => $row['kategori_produk'],
-                'kategori_produk' => $row['kategori_produk'],
-                'harga_beli'      => $row['harga_beli'],
-                'harga_beli'      => $row['harga_beli'],
-                'harga_jual'      => $row['harga_jual'],
-                'harga_jual'      => $row['harga_jual'],
-                'bisa_dijual'     => $row['bisa_dijual'],
-            ]);
-            // catat id dari buku yang baru dibuat
-            array_push($produk_id, $produk->produk_id);
-        }
-        // Ambil semua buku yang baru dibuat
-        $produks = Produk::whereIn('produk_id', $produk_id)->get();
+            // Variable array kosong untuk menampung pesan error
+            $errorMsg = [];
+            // Membuang spasi dan mengubah huruf menjadi lowercase (huruf kecil)
+            $bisaDijual = trim(strtolower($row['bisa_dijual']));
+            if ($bisaDijual !== 'ya' && $bisaDijual !== 'tidak') {
+                $errorMsg['pesan'] = 'Nilai dari kolom Bisa Dijual hanya boleh berisi ya atau tidak';
+                return response()->json($errorMsg);
+            }
 
-        return redirect()->back();
+            /*
+            |---------------------------------------------------------------------------
+            | Mengecek nama kategori produk
+            |---------------------------------------------------------------------------
+            |
+            | Disini kita cek apakah kategori produk yang user inputkan didalam
+            | Excel ada atau belum pada Master Data Kategori Produk.
+            | Jika ternyata tidak ada maka akan dibuatkan yang baru dengan nama
+            | sesuai dengan yang diinputkan pada Excel
+             */
+
+            /* Begin Cek Kategori Produk */
+
+            // Membuat variable array kosong
+            $dataKategoriProduk = [];
+
+            // Mengambil semua nama kategori produk pada tabel kategori_produks
+            // dan kemudian mengubahnya menjadi associative array
+            $namaKategoriProduk = KategoriProduk::select('nama_kategori_produk')->get();
+            $namaKategoriProduk = json_decode($namaKategoriProduk, true);
+
+            // Mengambil semua id kategori produk pada tabel kategori_produks
+            // dan kemudian mengubahnya menjadi associative array
+            $idKategoriProduk = KategoriProduk::select('id')->get();
+            $idKategoriProduk = json_decode($idKategoriProduk, true);
+
+            $importNamaKategoriProduk = strtolower($row['kategori_produk']);
+
+            // Memasukkan data ke variable array kosong
+            // yang telah dibuat diatas
+            array_push($dataKategoriProduk, $namaKategoriProduk, $idKategoriProduk);
+
+            // data nama dan id kategori produk "nama => id"
+            $arrNamaIdKategoriProduk = [];
+
+            // Mengisi variable array $arrNamaIdKategoriProduk dengan nama KategoriProduk sebagai index
+            // dan id KategoriProduk sebagai value
+            for ($i = 0; $i < count($dataKategoriProduk[1]); $i++) {
+                $arrNamaIdKategoriProduk[$dataKategoriProduk[0][$i]['nama_kategori_produk']] = $dataKategoriProduk[1][$i]['id'];
+            }
+
+            // Mengubah semua key (nama KategoriProduk) menjadi huruf kecil
+            $arrNamaIdKategoriProduk = array_change_key_case($arrNamaIdKategoriProduk, CASE_LOWER);
+
+            // Membuat variable array kosong untuk menampung nama kategori produk
+            $arrayNamaKategoriProduk = [];
+            // Memasukkan data nama KategoriProduk ke variable $arrayNamaKategoriProduk
+            foreach ($namaKategoriProduk as $namaKP) {
+                array_push($arrayNamaKategoriProduk, $namaKP['nama_kategori_produk']);
+            }
+            // Membuat value dari $arrayNamaKategoriProduk menjadi huruf kecil semua
+            $arrayNamaKategoriProduk = array_map('strtolower', $arrayNamaKategoriProduk);
+
+            /* End Cek Kategori Produk*/
+
+            if (in_array($importNamaKategoriProduk, $arrayNamaKategoriProduk)) {
+
+                // buat produk baru
+                $produk = Produk::create([
+                    'kode_produk'         => $row['kode_produk'],
+                    'nama_produk'         => $row['nama_produk'],
+                    'kategori_produks_id' => $arrNamaIdKategoriProduk[$importNamaKategoriProduk],
+                    'harga_beli'          => $row['harga_beli'],
+                    'harga_jual'          => $row['harga_jual'],
+                    'bisa_dijual'         => $row['bisa_dijual'],
+                ]);
+
+            } else {
+                // Membuat kategori produk baru
+                $kategori_produk = KategoriProduk::create([
+                    // Membuat setiap awal kata pada nama aplikasi menggunakan huruf kapital
+                    'nama_kategori_produk' => ucwords($importNamaKategoriProduk),
+                ]);
+
+                // Mengambil data terbaru dari master data kategori produk
+                $idKategoriProdukTerbaru = DB::table('kategori_produks')->orderBy('id', 'desc')->limit(1)->first();
+
+                // Membuat produk baru
+                $produk = Produk::create([
+                    'kode_produk'         => $row['kode_produk'],
+                    'nama_produk'         => $row['nama_produk'],
+                    'kategori_produks_id' => $idKategoriProdukTerbaru->id,
+                    'harga_beli'          => $row['harga_beli'],
+                    'harga_jual'          => $row['harga_jual'],
+                    'bisa_dijual'         => $row['bisa_dijual'],
+                ]);}
+
+            // catat id dari buku yang baru dibuat
+            // array_push($produk_id, $produk->produk_id);
+
+        }
+
+        // Ambil semua produk yang baru dibuat
+        $produks = Produk::whereIn('produk_id', $produk_id)->get();
     }
 }
