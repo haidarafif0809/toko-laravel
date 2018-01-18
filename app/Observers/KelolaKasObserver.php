63ae228db69833f7147a78d7952ecaf084f3690f
khofiq <khoirulgeong123@gmail.com>
2018-01-18 13:25:45 +0700

    menambahkan type penjualan, id_transaksi dan toko_id pada kelola kas

--- app/Observers/KelolaKasObserver.php
+++ app/Observers/KelolaKasObserver.php
@@ -7,27 +7,28 @@ use App\TransaksiKas;
 
 class KelolaKasObserver
 {
-    public function creating(KelolaKas $KelolaKas)
+    // created setelah data observer terinsert but, creating insert terlebih dahulu
+    public function created(KelolaKas $KelolaKas)
     {
         if ($KelolaKas->type == 1) {
 
-            TransaksiKas::create(['id' => $KelolaKas->kelola_kas_id, 'jenis_transaksi' => $KelolaKas->type, 'jumlah_masuk' => $KelolaKas->jumlah]);
+            TransaksiKas::create(['id_transaksi' => $KelolaKas->kelola_kas_id, 'toko_id' => $KelolaKas->toko_id, 'jenis_transaksi' => $KelolaKas->type, 'jumlah_masuk' => $KelolaKas->jumlah]);
         } else {
-            TransaksiKas::create(['id' => $KelolaKas->kelola_kas_id, 'jenis_transaksi' => $KelolaKas->type, 'jumlah_keluar' => $KelolaKas->jumlah]);
+            TransaksiKas::create(['id_transaksi' => $KelolaKas->kelola_kas_id, 'toko_id' => $KelolaKas->toko_id, 'jenis_transaksi' => $KelolaKas->type, 'jumlah_keluar' => $KelolaKas->jumlah]);
         }
     }
 
     public function updating(KelolaKas $KelolaKas)
     {
         if ($KelolaKas->type == 1) {
-            TransaksiKas::where('id', $KelolaKas->kelola_kas_id)->update(['jumlah_masuk' => $KelolaKas->jumlah]);
+            TransaksiKas::where('id_transaksi', $KelolaKas->kelola_kas_id)->update(['jumlah_masuk' => $KelolaKas->jumlah]);
         } else {
-            TransaksiKas::where('id', $KelolaKas->kelola_kas_id)->update(['jumlah_keluar' => $KelolaKas->jumlah]);
+            TransaksiKas::where('id_transaksi', $KelolaKas->kelola_kas_id)->update(['jumlah_keluar' => $KelolaKas->jumlah]);
         }
     }
 
     public function deleting(KelolaKas $KelolaKas)
     {
-        TransaksiKas::where('id', $KelolaKas->kelola_kas_id)->delete();
+        TransaksiKas::where('id_transaksi', $KelolaKas->kelola_kas_id)->delete();
     }
 }
