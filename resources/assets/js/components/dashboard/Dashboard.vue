3eb95ac0af2aaf1a2ea1de00794fa2c01dd3de72
khofiq <khoirulgeong123@gmail.com>
2018-01-27 14:18:21 +0700

    buat chart dengan vue-chart

--- resources/assets/js/components/dashboard/Dashboard.vue
+++ resources/assets/js/components/dashboard/Dashboard.vue
@@ -70,10 +70,13 @@ h4 {
 </style>
 <template>
   <div class="container">
+    <div class="panel panel-default">
+      <h4 class="panel-heading"><B>DASHBOARD</B></h4>
+    </div>
     <div class="row">
       <div class="">
         <p>{{ msg }}</p>
-        <div id="chartdiv" style="width: 100%; height: 400px;"></div>
+        <line-chart></line-chart>
       </div>
     </div>
     <div class="row">
@@ -162,116 +165,5 @@ export default {
       msg: 'Laporan Penjualan per hari'
     }
   },
-  created () {
-   var chart = AmCharts.makeChart("chartdiv", {
-    "type": "serial",
-    "theme": "light",
-    "marginRight": 40,
-    "marginLeft": 40,
-    "autoMarginOffset": 20,
-    "mouseWheelZoomEnabled":true,
-    "dataDateFormat": "YYYY-MM-DD",
-    "valueAxes": [{
-      "id": "v1",
-      "axisAlpha": 0,
-      "position": "left",
-      "ignoreAxisWidth":true
-    }],
-    "balloon": {
-      "borderThickness": 1,
-      "shadowAlpha": 0
-    },
-    "graphs": [{
-      "id": "g1",
-      "balloon":{
-        "drop":true,
-        "adjustBorderColor":false,
-        "color":"#ffffff"
-      },
-      "bullet": "round",
-      "bulletBorderAlpha": 1,
-      "bulletColor": "#FFFFFF",
-      "bulletSize": 5,
-      "hideBulletsCount": 50,
-      "lineThickness": 2,
-      "title": "red line",
-      "useLineColorForBulletBorder": true,
-      "valueField": "value",
-      "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
-    }],
-    "chartScrollbar": {
-      "graph": "g1",
-      "oppositeAxis":false,
-      "offset":30,
-      "scrollbarHeight": 80,
-      "backgroundAlpha": 0,
-      "selectedBackgroundAlpha": 0.1,
-      "selectedBackgroundColor": "#888888",
-      "graphFillAlpha": 0,
-      "graphLineAlpha": 0.5,
-      "selectedGraphFillAlpha": 0,
-      "selectedGraphLineAlpha": 1,
-      "autoGridCount":true,
-      "color":"#AAAAAA"
-    },
-    "chartCursor": {
-      "pan": true,
-      "valueLineEnabled": true,
-      "valueLineBalloonEnabled": true,
-      "cursorAlpha":1,
-      "cursorColor":"#258cbb",
-      "limitToGraph":"g1",
-      "valueLineAlpha":0.2,
-      "valueZoomable":true
-    },
-    "valueScrollbar":{
-      "oppositeAxis":false,
-      "offset":50,
-      "scrollbarHeight":10
-    },
-    "categoryField": "date",
-    "categoryAxis": {
-      "parseDates": true,
-      "dashLength": 1,
-      "minorGridEnabled": true
-    },
-    "export": {
-      "enabled": true
-    },
-    "dataProvider": [{
-      "date": "2012-07-27",
-      "value": 13
-    }, {
-      "date": "2012-07-28",
-      "value": 11
-    }, {
-      "date": "2012-07-29",
-      "value": 15
-    }, {
-      "date": "2012-07-30",
-      "value": 16
-    }, {
-      "date": "2012-07-31",
-      "value": 18
-    }, {
-      "date": "2012-08-01",
-      "value": 13
-    }, {
-      "date": "2012-08-02",
-      "value": 22
-    }, {
-      "date": "2012-08-03",
-      "value": 50
-    }]
-  });
-
-   chart.addListener("rendered", zoomChart);
-
-   zoomChart();
-
-   function zoomChart() {
-    chart.zoomToIndexes(chart.dataProvider.length - 40, chart.dataProvider.length - 1);
-  }
-}
 }
 </script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -295,6 +295,9 @@
 <script src="https://www.amcharts.com/lib/3/serial.js"></script>
 <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
 <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
+<script src="{{ asset('js/Chart.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+<script src="https://unpkg.com/vue-chartjs/dist/vue-chartjs.min.js"></script>
 <script type="text/javascript">
     function sweetAlert(){
         var namaToko = document.getElementById('nama_toko').value;
