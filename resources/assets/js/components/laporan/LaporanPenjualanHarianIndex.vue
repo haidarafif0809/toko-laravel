54df18d185cd77cb9a43c71fa5648b6914dd883b
galang <galangcahyapamungkas@gmail.com>
2018-02-15 10:12:26 +0700

    laporan harian penjualan kavepos

--- resources/assets/js/components/laporan/LaporanPenjualanHarianIndex.vue
+++ resources/assets/js/components/laporan/LaporanPenjualanHarianIndex.vue
@@ -40,31 +40,39 @@
 									</tr>
 								</thead>
 								<tbody >
-									<tr>
-										<td>2018-09-12</td>
-										<td align="right">1</td>
-										<td align="right">400.000</td>
-										<td align="right">400.000</td>
-									</tr>
-									<tr>
-										<td>2018-09-11</td>
-										<td align="right">2</td>
-										<td align="right">500.000</td>
-										<td align="right">250.000</td>
-									</tr>
-									<tr>
-										<td>2018-09-10</td>
-										<td align="right">3</td>
-										<td align="right">300.000</td>
-										<td align="right">100.000</td>
+									<tr v-for="laporan_penjualan_harians ,index in laporan_penjualan_harian">
+										<td>
+											{{ laporan_penjualan_harians.tanggal }}
+										</td>
+										<td align="right">
+											{{ new Intl.NumberFormat().format(laporan_penjualan_harians.jumlah_penjualan) }}
+										</td>
+										<td align="right">
+											{{ new Intl.NumberFormat().format(laporan_penjualan_harians.total_pembayaran) }}
+										</td>
+										<td align="right">
+											{{ new Intl.NumberFormat().format(laporan_penjualan_harians.total_pembayaran / laporan_penjualan_harians.jumlah_penjualan) }}
+										</td>
 									</tr>
 								</tbody>
 								<tfoot bgcolor="#d6d6c2">	
 									<tr>
 										<td><b><font color="#000000">Grand Total:</font></b></td>
-										<td align="right"><b><font color="#000000">6</font></b></td>
-										<td align="right"><b><font color="#000000">1.200.000</font></b></td>
-										<td align="right"><b><font color="#000000">200.000</font></b></td>
+										<td align="right"><b><font color="#000000">
+											{{ new Intl.NumberFormat().format(grand_total_penjualan.total_penjualan) }}
+										</font></b></td>
+										<td align="right"><b><font color="#000000">
+											{{ new Intl.NumberFormat().format(grand_total_penjualan.total_pembayaran) }}
+										</font></b></td>
+										<td v-if="grand_total_penjualan.total_penjualan > 0" align="right"><b><font color="#000000">
+											{{ (grand_total_penjualan.total_pembayaran / grand_total_penjualan.total_penjualan) }}
+										</font></b></td>
+										<td v-else align="right"> <b><font color="#000000">
+											0
+										</font></b></td>
+										<!-- <td align="right"><b><font color="#000000">
+											{{ (grand_total_penjualan.total_pembayaran / grand_total_penjualan.total_penjualan).toFixed(2) }}
+										</font></b></td> -->
 									</tr>
 								</tfoot>
 							</table>
@@ -75,6 +83,37 @@
 		</div>
 	</div>
 </template>
-<script >
-
+<script>
+export default{
+	data: function(){
+		return{
+			laporan_penjualan_harian:[],
+			grand_total_penjualan:[],
+			url : window.location.origin + (window.location.pathname).replace("home", "laporan"),
+		}
+	},
+	mounted(){
+		var app = this;
+		app.getDataLaporan();
+		app.getDataGrandTotal();
+	},
+	methods: {
+		getDataLaporan(){
+			let app = this;
+			axios.get(app.url+'/laporan-penjualan-harian/')
+			.then(function (resp){
+				app.laporan_penjualan_harian = resp.data;
+				console.log(resp.data)
+			})
+		},
+		getDataGrandTotal(){
+			let app = this;
+			axios.get(app.url+'/grand-total-penjualan/')
+			.then(function (resp){
+				app.grand_total_penjualan = resp.data;
+				console.log(resp.data)
+			})
+		}
+	}
+}
 </script>
