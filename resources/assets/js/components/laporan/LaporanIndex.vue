9f858a022d6cc1f56732e36fd3bf6750e104e289
galangcahya17 <galangcahyapamungkas@gmail.com>
2018-05-01 14:11:48 +0700

    menghaapus menu laporan yang tidak penting

--- resources/assets/js/components/laporan/LaporanIndex.vue
+++ resources/assets/js/components/laporan/LaporanIndex.vue
@@ -121,7 +121,7 @@ i{margin-bottom: 20px}
 						</div>
 					</router-link>
 				</div>
-				<div class="col-md-4">
+				<!-- <div class="col-md-4">
 					<router-link :to="{name: 'indexDashboard'}">
 						<div class="box">
 							<i class="fa fa-fire fa-3x"></i>
@@ -129,7 +129,7 @@ i{margin-bottom: 20px}
 							<p class="keterangan">Menampilkan laporan penerimaan pajak per transaksi.</p>
 						</div>
 					</router-link>
-				</div>
+				</div> -->
 				<div class="col-md-4">
 					<router-link :to="{name: 'indexLaporanDiskon'}">
 						<div class="box">
@@ -139,7 +139,7 @@ i{margin-bottom: 20px}
 						</div>
 					</router-link>
 				</div>
-				<div class="col-md-4">
+				<!-- <div class="col-md-4">
 					<router-link :to="{name: 'indexDashboard'}">
 						<div class="box">
 							<i class="fa fa-user fa-3x"></i>
@@ -147,8 +147,8 @@ i{margin-bottom: 20px}
 							<p class="keterangan">Menampilkan laporan jumlah pembelian member.</p>
 						</div>
 					</router-link>
-				</div>
-				<div class="col-md-4">
+				</div> -->
+				<!-- <div class="col-md-4">
 					<router-link :to="{name: 'indexDashboard'}">
 						<div class="box">
 							<i class="fa fa-rss fa-3x"></i>
@@ -156,7 +156,7 @@ i{margin-bottom: 20px}
 							<p class="keterangan">Menampilkan laporan laba per hari.</p>
 						</div>
 					</router-link>
-				</div>
+				</div> -->
 				<!-- <div class="col-md-4">
 					<router-link :to="{name: 'indexDashboard'}">
 						<div class="box">
