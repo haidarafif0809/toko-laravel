81e855a858cf2e89263051d756267e739bbb825a
khofiq <khoirulgeong123@gmail.com>
2018-05-07 13:42:52 +0700

    patch public in index.php

--- public/index.php
+++ public/index.php
@@ -36,6 +36,10 @@ require __DIR__ . '/../vendor/autoload.php';
  */
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+// set the public path to this directory
+$app->bind('path.public', function () {
+    return __DIR__;
+});
 
 /*
 |--------------------------------------------------------------------------
